c2626c6ac63b1fa059f2662637a1191a9d5807ed
prisc <priscillacamposzamora@gmail.com>
2024-03-08 12:05:48 -0600

    Corrección por error de  asignación procedural dentro de always_comb.

--- Ejercicio4/seven_segment_decoder.sv
+++ Ejercicio4/seven_segment_decoder.sv
@@ -18,39 +18,39 @@ module seven_segment_decoder(
     
         // 1: segment is off, 0: segment is on
     
-        4'b0000: begin assign segments = 7'b1111111; end // 0
+        4'b0000: segments = 7'b1111111; // 0
          
-        4'b0001: begin assign segments = 7'b1001111; end // 1
+        4'b0001: segments = 7'b1001111; // 1
             
-        4'b0010: begin assign segments = 7'b0010010; end // 2
+        4'b0010: segments = 7'b0010010; // 2
             
-        4'b0011: begin assign segments = 7'b0000110; end // 3
+        4'b0011: segments = 7'b0000110; // 3
             
-        4'b0100: begin assign segments = 7'b1001100; end // 4
+        4'b0100: segments = 7'b1001100; // 4
         
-        4'b0101: begin assign segments = 7'b0100100; end // 5
+        4'b0101: segments = 7'b0100100; // 5
             
-        4'b0110: begin assign segments = 7'b0100000; end // 6
+        4'b0110: segments = 7'b0100000; // 6
             
-        4'b0111: begin assign segments = 7'b0001111; end // 7
+        4'b0111: segments = 7'b0001111; // 7
             
-        4'b1000: begin assign segments = 7'b0000000; end // 8
+        4'b1000: segments = 7'b0000000; // 8
             
-        4'b1001: begin assign segments = 7'b0001100; end // 9
+        4'b1001: segments = 7'b0001100; // 9
             
-        4'b1010: begin assign segments = 7'b0001000; end // A
+        4'b1010: segments = 7'b0001000; // A
             
-        4'b1011: begin assign segments = 7'b0000000; end // B
+        4'b1011: segments = 7'b0000000; // B
             
-        4'b1100: begin assign segments = 7'b0110001; end // C
+        4'b1100: segments = 7'b0110001; // C
             
-        4'b1101: begin assign segments = 7'b0000001; end // D       
+        4'b1101: segments = 7'b0000001; // D       
         
-        4'b1110: begin assign segments = 7'b0110000; end // E
+        4'b1110: segments = 7'b0110000; // E
 
-        4'b1111: begin assign segments = 7'b0111000; end // F
+        4'b1111: segments = 7'b0111000; // F
         
-        default: begin assign segments = 7'b1111111; end // 0
+        default: segments = 7'b1111111; // 0
             
     endcase
     
