db98bd35fb1a16ec573f74260c81c4e8d5b31420
prisc <priscillacamposzamora@gmail.com>
2024-03-07 19:10:32 -0600

    Módulos internos del sistema.

---
+++ Ejercicio4/register_16b.sv
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////////
+// 
+// Registro de 16 bits de entrada y salida paralela, con habilitación de escritura.
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module register_16b(
+    input logic clk,
+    input logic WE,
+    input logic [15:0] D,
+    output logic [15:0] Q
+    );
+    
+    logic [15:0] register;
+    
+    always@(posedge clk) begin
+        if (WE) begin
+            register <= D;
+        end
+    end
+    
+    assign Q = register;
+    
+endmodule
