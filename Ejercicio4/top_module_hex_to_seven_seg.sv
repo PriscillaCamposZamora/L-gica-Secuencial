13724b718c6fcc010c2d6ceec4cce47b8a20308f
prisc <priscillacamposzamora@gmail.com>
2024-03-08 13:14:47 -0600

    Nueva salida dp para punto decimal del display.

--- Ejercicio4/top_module_hex_to_seven_seg.sv
+++ Ejercicio4/top_module_hex_to_seven_seg.sv
@@ -9,7 +9,8 @@ module top_module_hex_to_seven_seg(
     input logic clk,
     input logic rst,
     output logic [3:0] anode,
-    output logic [6:0] segments
+    output logic [6:0] segments,
+    output logic dp
     );
     
     logic write_enable;
@@ -22,4 +23,6 @@ module top_module_hex_to_seven_seg(
     
     seven_segment_display_controller(.clk(clk) , .hex_num_4digit(hex_num_4digit), .anode(anode), .segments(segments));
     
+    assign dp = 1;
+
 endmodule
