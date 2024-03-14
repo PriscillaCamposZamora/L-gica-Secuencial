6a75682129d572d7f750c95183ac4cb8c5628e1c
prisc <priscillacamposzamora@gmail.com>
2024-03-14 16:23:57 -0600

    Correción tamaño entradas y asignaciones blocking / nonblocking mezcladas.

--- Ejercicio6/register_file.sv
+++ Ejercicio6/register_file.sv
@@ -10,23 +10,22 @@ module register_file #(parameter n = 3, parameter w = 8)(
     input logic rst,
     input logic WE,
     input logic [w-1:0] data_in,
-    input logic [(2**n)-1:0] add_data_in,
-    input logic [(2**n)-1:0] add_data_out1,
-    input logic [(2**n)-1:0] add_data_out2,
+    input logic [n-1:0] add_data_in,
+    input logic [n-1:0] add_data_out1,
+    input logic [n-1:0] add_data_out2,
     output logic [w-1:0] data_out1,
     output logic [w-1:0] data_out2
     );
     
     logic [(2**n)-1:0] [w-1:0] registers;
     
-    assign registers[0] = {w{1'b0}};
-    
     always@(posedge clk) begin
+        registers[0] <= {w{1'b0}};
         if (WE&&(add_data_in!=0)) begin
             registers[add_data_in] <= data_in;
         end
         if (rst) begin
-            registers[(2**n)-1:1] = {w{1'b0}};
+            registers[(2**n)-1:1] <= {w{1'b0}};
         end
     end
     
