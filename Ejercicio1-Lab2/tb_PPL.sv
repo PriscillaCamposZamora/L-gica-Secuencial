e55a1432e15a2913e37021b10b9bd49ae626fc25
Siuuu <111307104+Siuuuuuuu07@users.noreply.github.com>
2024-03-16 08:50:38 -0600

    Mejoras y correcciones

--- Ejercicio1-Lab2/tb_PPL.sv
+++ Ejercicio1-Lab2/tb_PPL.sv
@@ -3,38 +3,43 @@
 module tb_clocking_wizard;
 
   // Parámetros del reloj
-  parameter CLK_PERIOD = 0.000000001; // Periodo de reloj en unidades de tiempo, con f=100Mhz
+  parameter CLK_PERIOD_IN = 10; // Periodo de reloj en unidades de tiempo, con f=100Mhz
+ 
   
   // Señales de reloj
-  reg CLK_100MHz;
-  initial begin
-    CLK_100MHz = 0;
-    forever #((CLK_PERIOD)/2) CLK_100MHz = ~CLK_100MHz; // Generar el reloj
-  end
-  
+  reg CLK_in_100MHz;//reloj de 100Mhz
+  reg CLK_OUT_10MHz;//reloj de 10MHz
+
 //instanciado del clocking Wizard   
     clk_wiz_0 instance_name
    (
     // Clock out ports
-    .CLK_10MHz(CLK_10MHz),     // output CLK_10MHz
+    .CLK_OUT_10MHz(CLK__OUT_10MHz),     // output CLK_10MHz
     // Status and control signals
     .reset(reset), // input reset
     .locked(locked),       // output locked
    // Clock in ports
-    .CLK_100MHz(CLK_100MHz)      // input CLK_100MHz
+    .CLK_in_100MHz(CLK_in_100MHz)      // input CLK_100MHz
 );
 //Aqui termina el instanciado del clocking Wizard
 
-
-  // Aquí debo agregar las señales de prueba y lógica de control
-
-  // Aqui empieza la simulación del comportamiento
+  // Simular el comportamiento
   initial begin
-    #1000; // Simulación inicial
-  // Cambiar la frecuencia de entrada
- // clk_wiz_0 instance_name.clk_in_period = new_period_value; esta linea tira error
-  #1000; // Continuar simulación con nueva frecuencia de entrada
-    $finish; // Terminar la simulación
+    // Inicialización de señales
+    CLK_in_100MHz = 0;
+    CLK_OUT_10MHz = 0;
+    
+    // Generar el reloj de entrada de 100 MHz
+    forever #((CLK_PERIOD_IN)/2) CLK_in_100MHz = ~CLK_in_100MHz; 
+    
+    // Esperar 5 ciclos para asegurar la estabilidad del reloj
+    #5;
+    
+    // Simular durante un período de tiempo suficiente
+    #1000;
+    
+    // Terminar la simulación
+    $finish;
   end
 
 endmodule
